1c1a0689d87de4a0aff9182ade4b036bee1d3c32
Vincent Wetzel <vincent.wetzel14@gmail.coms>
2017-04-02 18:55:49 -0400

    messed up

--- ques2.js
+++ ques2.js
@@ -1,4 +1,9 @@
 //functions//
+var vinnie= new Teacher ("male", 28, "Vinnie")
+var jeff= new Student ("male", 12, "Jeff")
+var kate= new Student ("female",14, "Kate")
+var judy= new Student ("female", 12. "Judy")
+var school = new School()
 
 function Person(gender,age,name){
 this.gender= gender
@@ -13,10 +18,12 @@ function Teacher(gender,age,name){
 function Student(gender, age, name){
   Person.call(this, gender, age, name)
 }
-  
+
 function School(){
   this.persons= []
 }
+
+
 //prototype objects//
 School.prototype.addPerson = function(person){
   this.persons.push(person)
@@ -27,3 +34,11 @@ Person.prototype.displayAllInfo = function(){
 }
 Teacher.prototype = Object.create(Person.prototype)
 Student.prototype = Object.create(Person.prototype)
+
+school.addPerson(judy)
+school.addPerson(kate)
+school.addPerson(vinnie)
+school.addPerson(jeff)
+
+console.log(vinnie.displayAllInfo)
+console.log(judy.displayAllInfo)
